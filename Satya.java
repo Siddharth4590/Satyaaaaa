a2d7a7b9de8591552cf1c680d34cfd83192051e8
Siddharth4590 <siddharthjagt253@gmail.com>
2024-12-27 15:01:42 +0530

    Update Satya.java

--- Satya.java
+++ Satya.java
@@ -1,5 +1,7 @@
 public class Satya{
     public static void main(String args[]){
         System.out.println("Landya");
+        System.out.println("pakistan ");
+        System.out.println("Mullya");
     }
 }
